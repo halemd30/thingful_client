0dc597168406675d0875c81dba432679901f88e6
Mac Hale <machaledesign@gmail.com>
2020-05-10 13:18:40 -0600

    first commit

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Button, Input } from '../Utils/Utils'
+import TokenService from '../../services/token-service'
 
 export default class LoginForm extends Component {
   static defaultProps = {
@@ -11,9 +12,14 @@ export default class LoginForm extends Component {
   handleSubmitBasicAuth = ev => {
     ev.preventDefault()
     const { user_name, password } = ev.target
+    // saveCredentials(
+    //   window.btoa(userName.value + ':' + password.value)
+    // )
 
-    console.log('login form submitted')
-    console.log({ user_name, password })
+    // makes token and saves it in one step
+    TokenService.saveAuthToken(
+      TokenService.makeBasicAuthToken(user_name.value, password.value)
+    )
 
     user_name.value = ''
     password.value = ''
